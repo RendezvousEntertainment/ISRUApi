e38da407785dca2931b5a3473b0204be2fc6cdc9
pasalvetti <pasalvetti@gmail.com>
2025-05-01 16:52:30 +0200

    Only displays resources that have been scanned (2/2)

--- src/ISRUApi/UI/CBResourceChart.cs
+++ src/ISRUApi/UI/CBResourceChart.cs
@@ -2,9 +2,10 @@
 
 namespace ISRUApi.UI
 {
-    public class CBResourceChart(string resource, Texture2D map = null)
+    public class CBResourceChart(string resource, Texture2D map = null, bool isScanned = false)
     {
         public string ResourceName { get; } = resource;
         public Texture2D LevelMap { get; set; } = map;
+        public bool IsScanned { get; set; } = isScanned;
     }
 }
